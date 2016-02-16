5ed08d7ef95e5b624645c24b84f1c32ec7427174
Steven Bambling <smbambling@gmail.com>
2016-02-16 10:36:42 -0500

    updates with ssh_check

--- idrac_jumper.py
+++ idrac_jumper.py
@@ -4,10 +4,11 @@
 import sys
 import argparse
 import logging
+import paramiko
 
 # Gather our code in a main() function
 def main(args, loglevel):
-    logging.basicConfig(format="%(levelname)s: %(message)s", level=loglevel)
+    logging.basicConfig(format="%(levelname)s: %(message)s", level=loglevel) 
 
     # Set variables from ARGS
     drac = args.drac
@@ -19,10 +20,26 @@ def main(args, loglevel):
     logging.info("You passed an argument.")
     logging.debug("Your Argument: %s" % drac)
 
+    check_ssh(jumper)
+
+def check_ssh(jumper):
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(jumper)
+        return True
+    except (BadHostKeyException, AuthenticationException, 
+        SSHException, socket.error) as e:
+        print e
+        #logging.basicConfig(format="%(levelname)s: %(message)s", level=loglevel)
+        #logging.info("ERROR: %s" % e)
+
 # Call the main() function to begin the program.
 if __name__ == '__main__':
     parser = argparse.ArgumentParser( 
                                       description = "forwards port 443 and 5900 via SSH for iDrac communication",
+                                      usage='use "%(prog)s (-h, --help)" for more information',
+                                      formatter_class=argparse.RawTextHelpFormatter,
                                       epilog = "")
     # TODO Specify your real parameters here.
     parser.add_argument(
@@ -34,6 +51,14 @@ if __name__ == '__main__':
     group.add_argument(
                        "-s",
                        "--site",
+                       choices=(
+                           'ashcore',
+                           'ashdmz',
+                           'chacore',
+                           'chadmz',
+                           'sea',
+                           'sjc',
+                           'iad'),
                        type=str,
                        help="the site abbr. for the MGMT network you want to connect to")
     group.add_argument(
@@ -49,7 +74,7 @@ if __name__ == '__main__':
                         required=True)
     args = parser.parse_args()
     
-    # Setup logging
+    # Setup logging level
     if args.verbose:
       loglevel = logging.DEBUG
     else:
